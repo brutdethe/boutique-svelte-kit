83f800bd79fe5a048882a1ffed4a5558eba1a15f
pntbr <pntbr@pm.me>
2023-03-09 09:26:45 +0100

    refactor stores

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -5,16 +5,16 @@ import {
     PUBLIC_github_data_repo
 } from '$env/static/public'
 
-export const setup = writable(async() => {
-    const res = await fetch(`https://raw.githubusercontent.com/${PUBLIC_github_data_repo}/main/setup.json`)
-    const setup = await res.json()
+const githubRepoName = PUBLIC_github_data_repo
 
-    return setup
-})
+const getGithubUrl = (repo, file) =>
+    `https://raw.githubusercontent.com/${repo}/main/${file}`
 
-export const products = writable(async() => {
-    const res = await fetch(`https://raw.githubusercontent.com/${PUBLIC_github_data_repo}/main/produits.json`)
-    const products = await res.json()
+async function loadData(repo, file) {
+    const res = await fetch(getGithubUrl(repo, file))
+    return await res.json()
+}
 
-    return products
-})
+export const setup = writable(async() => loadData(githubRepoName, 'setup.json'))
+export const categories = writable(async() => loadData(githubRepoName, 'categories.json'))
+export const products = writable(async() => loadData(githubRepoName, 'produits.json'))
--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,13 +2,14 @@ import {
     get
 } from 'svelte/store'
 import {
-    products
+    products,
+    categories
 } from '$lib/stores.js'
 
 /** @type {import('../../.svelte-kit/types/src/routes/$types').PageLoad} */
 export async function load() {
     const data = await get(products)()
-
+    console.log("categories", await get(categories)())
     return {
         data
     }
