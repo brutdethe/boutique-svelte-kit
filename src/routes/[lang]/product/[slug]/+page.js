89409e9b028be5f1d73e13dffcb8750a2976f528
pntbr <pntbr@pm.me>
2023-03-24 12:48:47 +0100

    display a specific message when no-product

--- src/routes/[lang]/product/[slug]/+page.js
+++ src/routes/[lang]/product/[slug]/+page.js
@@ -4,9 +4,6 @@ import {
 import {
     products
 } from '$lib/stores.js'
-import {
-    error
-} from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({
@@ -14,13 +11,9 @@ export async function load({
 }) {
     const productId = params.slug.split("_")[1]
     const productz = await get(products)()
-    const product = productz.filter(item => item.id === productId)
-
-    if (!product.length) {
-        throw error(404, 'Not found')
-    }
+    const product = productz.filter(item => item.id === productId)[0]
 
     return {
-        product: product[0]
+        product: product
     }
 }
