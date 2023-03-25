69543a4d0599f19e4b87c1174b8b0d6cc2d2bb96
pntbr <pntbr@pm.me>
2023-03-25 20:35:44 +0100

    stylise error 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,3 +1,14 @@
+<script>
+  import {language} from '$lib/stores.js'
+
+  const dict = {
+    texte: {
+      en: 'This page does not exists',
+      fr: 'Cette page n\'existe pas'
+    }
+  }
+</script>
+
 <style>
   .error {
     text-align: center;
@@ -11,14 +22,30 @@
     font-weight: 700;
     margin: 0 0 0.5em 0;
   }
+  img {
+      max-width: 100%;
+  }
   @media (min-width: 480px) {
     h1 {
       font-size: 4em;
     }
   }
+
+  @media (max-width: 600px) {
+    ul {
+        justify-content: center;
+    }
+  }
+
 </style>
 
+
+<svelte:head>
+	<title>Error 404</title>
+</svelte:head>
+
 <section class="error">
-    <h1>Error 404</h1>
+    <h1>404</h1>
+    <h4>{dict.texte[$language]}</h4>
     <img src="/img/404.jpg" alt="404" />
 </section>
