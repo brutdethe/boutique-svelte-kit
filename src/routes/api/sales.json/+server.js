9fef4f0df9ad29e26e3ca12337b131953d88150d
pntbr <pntbr@pm.me>
2023-03-31 00:03:24 +0200

    get stripe paidCheckoutSessionsId

--- src/routes/api/sales.json/+server.js
+++ src/routes/api/sales.json/+server.js
@@ -1,3 +1,40 @@
+import {
+    SECRET_stripe_sk
+} from '$env/static/private'
+import Stripe from 'stripe'
+
+async function getPaidsSessionsIds(stripe) {
+
+    async function getCheckoutSessions(starting_after = null) {
+        const options = {
+            limit: 100
+        }
+        if (starting_after) {
+            options.starting_after = starting_after
+        }
+
+        return await stripe.checkout.sessions.list(options)
+    }
+
+    function getPaidCheckoutsIds(checkoutsData) {
+        return checkoutsData.filter(session => session.payment_status === 'paid').map(session => session.id)
+    }
+
+    let has_more = true
+    let checkoutIDs = []
+    let lastIDs = null
+
+    while (has_more) {
+        let checkoutSessions = await getCheckoutSessions(lastIDs)
+        has_more = checkoutSessions.has_more
+        checkoutIDs = checkoutIDs.concat(getPaidCheckoutsIds(checkoutSessions.data))
+        lastIDs = checkoutSessions.data[checkoutSessions.data.length - 1].id
+    }
+
+    return checkoutIDs
+}
+
+
 export const GET = async({
     request,
     url
@@ -8,6 +45,11 @@ export const GET = async({
         "manontas44": 1,
         "puer2": 2
     }
+
+
+    const stripeKeySk = SECRET_stripe_sk
+    const stripe = new Stripe(stripeKeySk)
+    console.log("Stripe", await getPaidsSessionsIds(stripe))
 
     return new Response(JSON.stringify(sales), {
         status: 200
