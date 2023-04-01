1476c7fe399793987c273ab96e9676d4b534c670
pntbr <pntbr@pm.me>
2023-04-01 02:15:31 +0200

    display  json with products in stock

---
+++ src/routes/[lang]/tools.json/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let data
+
+    const products = data.productsWithStock.map(product => {
+        Reflect.deleteProperty(product, 'stock')
+
+        return product    
+    })
+</script>
+<style>
+  pre {
+    background-color: #f8f8f8;
+    border: 1px solid #ddd;
+    border-radius: 3px;
+    font-size: 14px;
+    padding: 10px;
+    overflow: auto;
+  }
+
+  code {
+    color: #444;
+  }
+</style>
+<pre>
+    <code>
+        {JSON.stringify(products, null, 4)}
+    </code>
+</pre>
