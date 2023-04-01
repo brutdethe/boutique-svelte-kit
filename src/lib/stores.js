a245c27e275514d0138f3863c6f849362f00d4e8
pntbr <pntbr@pm.me>
2023-04-01 16:59:22 +0200

    adds sales bak file to optimise product's loading

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -10,16 +10,17 @@ const githubRepoName = PUBLIC_github_data_repo
 const getGhUrl = (repo, file) =>
     `https://raw.githubusercontent.com/${repo}/main/${file}`
 
-let storedLanguage = 'fr'
-let storedCurrency = 'EUR'
-let storedCountry = 'france'
-let storedBasket = []
+const storedLanguage = 'fr'
+const storedCurrency = 'EUR'
+const storedCountry = 'france'
+const storedBasket = []
+const storedRate = 1.1
 
 export const language = writable(storedLanguage)
 export const categorySelected = writable(0)
 export const currency = writable(storedCurrency)
 export const country = writable(storedCountry)
 export const basket = writable(storedBasket)
-export const rate = writable(1.1)
+export const rate = writable(storedRate)
 
 export const getGithubUrl = writable((file) => getGhUrl(githubRepoName, file))
